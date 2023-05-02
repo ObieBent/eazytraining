05850c9853dfc9fae5064632b90f98b950e84565
Boris ASSOGBA <borisassogba@live.fr>
2023-05-02 03:11:11 +0200

    update

--- apps/sample-app-httpd/sample-app-httpd.sh
+++ apps/sample-app-httpd/sample-app-httpd.sh
@@ -7,7 +7,7 @@ declare -r NAMESPACE=${NAMESPACE:-eazytraining}
 
 _log() {
     local level=$1; shift
-    echo -e "$level: $@"
+    echo  "$level: $@"
 }
 
 log_err() {
@@ -27,15 +27,15 @@ err() {
 
 sample_app_validate_tools() {
   info "validating tools"
-
+  echo " "
   oc version  >/dev/null 2>&1 || err 1 "no oc binary found"
   return 0
 }
 
 bootstrapping() {
     sample_app_validate_tools
-
     info "ensure namespace $NAMESPACE exists"
+    echo " "
     oc get ns "$NAMESPACE" 2>/dev/null  || {
       oc new-project $NAMESPACE
     }
@@ -45,13 +45,15 @@ sample_app_deploy_build_config_pipeline() {
   cd "$SCRIPT_DIR/openshift/templates"
   bootstrapping
   info "Deploying Build Config Pipeline"
+  echo " "
   oc -n "$NAMESPACE" process -f sample-app-httpd.yaml | oc -n "$NAMESPACE" apply -f - 
-  echo -e "\nBuild Config Pipeline"
-  echo "=========================="
+  info "Build Config Pipeline"
+  echo "==============================================="
+  echo " "
 }
 
 sample_app_url(){
-  echo "Click the following URL to access the application"
+  info "Click the following URL to access the application"
   echo " "
   oc -n "$NAMESPACE" get route route-eazytraining-httpd --template='http://{{.spec.host}} '
   echo " " 
